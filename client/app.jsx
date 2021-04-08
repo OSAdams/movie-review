907ef126089feabc4fa118122efd356fa1383b40
Owen Adams <owenadams.main@gmail.com>
2021-04-08 18:51:02 -0400

    Created navigation bar with app title

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Home from './pages/home';
+import Navigation from './components/navigation';
 
 export default class App extends React.Component {
   render() {
-    return <Home />;
+    return (
+      <>
+        <Navigation />
+        <Home />
+      </>
+    );
   }
 }
---
+++ client/components/navigation.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const styles = {
+  nav: {
+    position: 'absolute',
+    background: '#49208C',
+    width: '100%',
+    height: '40px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    color: '#F5F5F5'
+  },
+  icon: {
+    color: '#F5F5F5',
+    position: 'absolute',
+    fontSize: '24px',
+    top: '7px',
+    left: '10px'
+  }
+};
+
+export default function Navigation(props) {
+  return (
+      <header className='header' style={ styles.nav }>
+        <div className='menu-icon' style={ styles.icon }>
+          <i className='fas fa-bars'></i>
+        </div>
+        <div className='title' style={ styles.title }>
+          <h2>Movie Review</h2>
+        </div>
+      </header>
+  );
+}
