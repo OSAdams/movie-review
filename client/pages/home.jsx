5524679cd4d72959c1cd1174fb8fb2d898463868
Owen Adams <owenadams.main@gmail.com>
2021-04-08 22:18:38 -0400

    added image to home page. #1

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 export default function Home(props) {
   return (
     <>
+      <div className='Logo'>
+        <img src='.../images/sa1.png' alt='Logo'></img>
+      </div>
     </>
   );
 }
