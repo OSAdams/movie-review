8f96dab528e78053a191978c3bd8ce62fa9b5f6f
Owen Adams <owenadams.main@gmail.com>
2021-04-09 13:58:12 -0400

    Added search box for movie titles. #1

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -2,14 +2,33 @@ import React from 'react';
 
 const styles = {
   container: {
-    width: '375px'
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  logo: {
+    marginTop: '1rem'
+  },
+  fontStyles: {
+    color: '#222222',
+    fontSize: '24px'
+  },
+  searchBox: {
+    background: '#6774ED',
+    border: 'none',
+    borderRadius: '2px',
+    height: '35px'
   }
 };
 
 export default function Home(props) {
   return (
-      <div className='container' styles={ styles.container }>
-        <img src='/images/logo.png' alt='Logo'></img>
+      <div className='container' style={ styles.container }>
+        <img src='/images/logo.png' alt='Logo' style={ styles.logo }></img>
+        <div className='search'>
+          <p style={ styles.fontStyles }>Search by Title</p>
+          <input type='search' placeholder='Search Movie Title' style={ styles.searchBox }></input>
+        </div>
       </div>
   );
 }
