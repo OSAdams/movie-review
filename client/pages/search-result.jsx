000eca19026654aed03f7c460977d524dfdc3d00
Owen Adams <owenadams.main@gmail.com>
2021-04-14 18:04:00 -0400

    Added styling for labels and data. #5

--- client/pages/search-result.jsx
+++ client/pages/search-result.jsx
@@ -22,11 +22,19 @@ const styles = {
   movieRow1: {
     display: 'flex'
   },
+  dataRow: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
   labelTxt: {
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    fontSize: '12px'
   },
   botPad: {
     paddingBottom: '3px'
+  },
+  defaultFont: {
+    fontSize: '12px'
   }
 };
 
@@ -51,7 +59,7 @@ class DisplayMovie extends React.Component {
 
   render() {
     if (!this.state.movie) return null;
-    const { Title, Poster, Plot } = this.state.movie;
+    const { Title, Poster, Plot, Genre } = this.state.movie;
     return (
       <div className='movie-container' style={ styles.movieContainer }>
 
@@ -59,15 +67,35 @@ class DisplayMovie extends React.Component {
 
           <div className='movie-row-1' style={ styles.movieRow1 }>
 
-            <div className='movie-poster' style={ Object.assign({}, styles.row1, styles.poster) }>
+            <div className='movie-poster' style={ Object.assign({},
+              styles.row1,
+              styles.poster) }>
 
               <img src={Poster} alt={Title} />
 
             </div>
 
-            <div className='movie-information' style={ styles.row1 }>
+            <div className='movie-row-1' style={ styles.row1 }>
 
-              <h3>{Title}</h3>
+              <div className='movie-title'>
+
+                <h3>{Title}</h3>
+
+              </div>
+
+              <div className='movie-data-col'>
+
+                <div className='movie-genre' styles={ Object.assign({},
+                  styles.botPad,
+                  styles.dataRow) }>
+
+                  <span className='label' style={ styles.labelTxt }>Genre:</span>
+
+                  <span className='value' style={ styles.defaultFont }>{Genre}</span>
+
+                </div>
+
+              </div>
 
             </div>
 
