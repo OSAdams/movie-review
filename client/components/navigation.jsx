cf3f20c801a9f432145f33c65df5960d4912ebeb
Owen Adams <owenadams.main@gmail.com>
2021-04-15 19:25:23 -0400

    Styling changes. #6

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -1,38 +1,16 @@
 import React from 'react';
 
-const styles = {
-  nav: {
-    position: 'static',
-    background: '#49208C',
-    width: '100%',
-    height: '40px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  title: {
-    color: '#F5F5F5'
-  },
-  icon: {
-    color: '#F5F5F5',
-    position: 'absolute',
-    fontSize: '24px',
-    top: '7px',
-    left: '10px'
-  }
-};
-
 export default function Navigation(props) {
   return (
-      <header className='header' style={ styles.nav }>
+      <header className='header'>
 
-        <div className='menu-icon' style={ styles.icon }>
+        <div className='menu-icon'>
 
           <i className='fas fa-bars'></i>
 
         </div>
 
-        <div className='title' style={ styles.title }>
+        <div className='app-name'>
 
           <h2>Movie Review</h2>
 
